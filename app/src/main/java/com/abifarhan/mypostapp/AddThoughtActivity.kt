de72383c8e09978405dc23d75f3bd7c937780908
abiFarhan <abifarhan04@gmail.com>
2021-07-22 14:31:26 +0700

    Proceed button category

--- app/src/main/java/com/abifarhan/mypostapp/AddThoughtActivity.kt
+++ app/src/main/java/com/abifarhan/mypostapp/AddThoughtActivity.kt
@@ -3,14 +3,29 @@ package com.abifarhan.mypostapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.abifarhan.mypostapp.databinding.ActivityAddThoughtBinding
 
 class AddThoughtActivity : AppCompatActivity() {
+
+    private var _binding: ActivityAddThoughtBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_thought)
+        _binding = ActivityAddThoughtBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
-    fun addFunnyClicked(view: View) {}
-    fun onSeriousButtonClicked(view: View) {}
-    fun onCrazyClicked(view: View) {}
+    fun addFunnyClicked(view: View) {
+        binding.addCrazzyButton.isChecked = false
+        binding.addSeriousButton.isChecked = false
+    }
+    fun onSeriousButtonClicked(view: View) {
+        binding.addCrazzyButton.isChecked = false
+        binding.addFunnyButton.isChecked = false
+    }
+    fun onCrazyClicked(view: View) {
+        binding.addSeriousButton.isChecked = false
+        binding.addFunnyButton.isChecked = false
+    }
 }
---
+++ app/src/main/java/com/abifarhan/mypostapp/Constanst.kt
@@ -0,0 +1,4 @@
+package com.abifarhan.mypostapp
+
+class Constanst {
+}
